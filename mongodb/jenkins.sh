f71a86d88cc131accc04f4694df2a8210c36fece
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2015-01-22 15:14:16 +0000

    Stop mounting host directory on to container

--- mongodb/jenkins.sh
+++ mongodb/jenkins.sh
@@ -3,4 +3,3 @@
 mkdir -p ~/mongodb/data
 wget --quiet -O ~/mongodb/data/zips.json http://media.mongodb.org/zips.json
 docker build mongodb/
-docker run -v /Users/ajlanghorn/Desktop:/mnt/mongodb 096454c06e32
