847682c3453c9c99ed71a376a5ed938bc7967a27
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2015-01-22 13:21:34 +0000

    Change container ID

--- mongodb/jenkins.sh
+++ mongodb/jenkins.sh
@@ -3,4 +3,4 @@
 mkdir -p ~/mongodb/data
 wget --quiet -O ~/mongodb/data/zips.json http://media.mongodb.org/zips.json
 docker build mongodb/
-docker run -v /Users/ajlanghorn/Desktop:/mnt/mongodb mongodb
+docker run -v /Users/ajlanghorn/Desktop:/mnt/mongodb 096454c06e32
