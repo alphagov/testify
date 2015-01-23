5af7921fb09f62e4447cfebd27e53286219ddfc3
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2015-01-23 10:36:34 +0000

    Docker rm *

--- mongodb/jenkins.sh
+++ mongodb/jenkins.sh
@@ -4,6 +4,7 @@ mkdir -p ~/mongodb/data
 wget --quiet -O ~/mongodb/data/zips.json http://media.mongodb.org/zips.json
 docker pull mongo
 docker pull ubuntu
+docker rm *
 docker run --name mongodb -d mongo
 docker run --name ubuntu -d ubuntu
 docker exec -it mongodb -d mongoimport --verbose --db zips --collection zips --type json --file mongodb/zips.json
