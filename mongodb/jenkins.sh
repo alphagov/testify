aa951eed531d6f3ce829f30d293f11fdca93f1ec
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2015-01-27 22:39:54 +0000

    Run MongoDB query/ies in new container
    Also tidies this section up with some linebreaks.

--- mongodb/jenkins.sh
+++ mongodb/jenkins.sh
@@ -19,8 +19,11 @@ echo "Removing any pre-existing Docker containers..."
 docker rm -f mongodb-host mongodb-client > /dev/null 2>&1
 echo "Starting up Docker container(s)..."
 docker run --name mongodb-host -d mongo
-mongohost_cid=$(docker -D inspect --format '{{ .Id }}' mongodb-host)
-mongohost_ip=$(docker -D inspect --format '{{ .NetworkSettings.IPAddress }}' mongodb-host)
-docker run -v $(pwd)/data:/data --name mongodb-client mongo mongoimport --quiet --host $mongohost_ip:27017 --db zips --collection zips --type json --file /data/zips.json
-mongoclient_cid=$(docker -D inspect --format '{{ .Id }}' mongodb-client)
-mongoclient_ip=$(docker -D inspect --format '{{ .NetworkSettings.IPAddress }}' mongodb-client)
+mongohost_cid=$(docker inspect --format '{{ .Id }}' mongodb-host)
+mongohost_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongodb-host)
+
+docker run -v $(pwd)/data:/data --name mongodb-client -d mongo mongoimport --quiet --host $mongohost_ip:27017 --db zips --collection zips --type json --file data/zips.json
+mongoclient_cid=$(docker inspect --format '{{ .Id }}' mongodb-client)
+mongoclient_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongodb-client)
+
+docker run -v $(pwd)/data:/data -ti mongo mongo --host $mongohost_ip --eval "printjson(db.zips.validate())"
