c8719677d214e1ebb8bc404031114931f4ff6874
Andrew Langhorn <andrew.langhorn@digital.cabinet-office.gov.uk>
2015-01-28 09:52:02 +0000

    First working iteration

--- mongodb/jenkins.sh
+++ mongodb/jenkins.sh
@@ -9,14 +9,18 @@ fi
 
 echo "Downloading MongoDB data..."
 wget --quiet -O $(pwd)/data/zips.json http://media.mongodb.org/zips.json
+
 if ls -lah data | grep tar.gz > /dev/null 2>&1; then
   echo "Backup is tarballed. Extracting..."
   tar -zxvf *.tar.gz
 fi
+
 echo "Pulling Docker image: MongoDB..."
 docker pull mongo > /dev/null 2>&1
+
 echo "Removing any pre-existing Docker containers..."
 docker rm -f mongodb-host mongodb-client > /dev/null 2>&1
+
 echo "Starting up Docker container(s)..."
 docker run --name mongodb-host -d mongo
 mongohost_cid=$(docker inspect --format '{{ .Id }}' mongodb-host)
@@ -26,4 +30,4 @@ docker run -v $(pwd)/data:/data --name mongodb-client -d mongo mongoimport --qui
 mongoclient_cid=$(docker inspect --format '{{ .Id }}' mongodb-client)
 mongoclient_ip=$(docker inspect --format '{{ .NetworkSettings.IPAddress }}' mongodb-client)
 
-docker run -v $(pwd)/data:/data -ti mongo mongo --host $mongohost_ip --eval "printjson(db.zips.validate())"
+docker run -v $(pwd)/data:/data -ti mongo mongo --host $mongohost_ip --eval "printjson(db.stats())"
